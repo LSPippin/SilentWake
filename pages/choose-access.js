05be63aa4e75c10aaaa6740c78e266d06842ae47
HnL0011 <hlabarge1@live.maryville.edu>
2025-07-24 17:44:54 -0500

    Add Choose Your Access page

---
+++ pages/choose-access.js
@@ -0,0 +1,8 @@
+export default function ChooseAccess() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Choose Your Access</h1>
+      <p>Select how you want to engage with Silent Wake.</p>
+    </div>
+  );
+}
