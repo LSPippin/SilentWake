e82c2dae0cea25ce378d5e35a9b2857abfd1ebb1
HnL0011 <hlabarge1@live.maryville.edu>
2025-07-24 18:26:50 -0500

    Delete pages directory

--- pages/choose-access.js
+++
@@ -1,8 +0,0 @@
-export default function ChooseAccess() {
-  return (
-    <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>Choose Your Access</h1>
-      <p>Select how you want to engage with Silent Wake.</p>
-    </div>
-  );
-}
