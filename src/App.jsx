477abee8712b7e015acacdfca01cdcefb7a74ac5
HnL0011 <hlabarge1@live.maryville.edu>
2025-07-31 17:04:36 +0000

    Add submit report to apps

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SplashScreen from './pages/SplashScreen';
 import ChooseAccess from './pages/ChooseAccess';
 import Onboarding from './pages/Onboarding';
+import ReportForm from './components/ReportForm';
 
 // import other pages as needed
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/welcome" element={<div>Welcome page coming soon!</div>} />
         <Route path="/onboarding" element={<Onboarding />} />
         <Route path="/choose-access" element={<ChooseAccess />} />
+        <Route path="/submit-report" element={<ReportForm />} />
       </Routes>
     </Router>
   );
